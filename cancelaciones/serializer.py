4817638e2f2af8a8e125649ffd8e3374ebd32a01
DEVBRISA <devbrisanatural@gmail.com>
2025-07-07 22:09:30 -0500

    cancelar

--- cancelaciones/serializer.py
+++ cancelaciones/serializer.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
+
+from reservas.serializer import ReservaSerializer
 from .models import CancelacionReserva
 
 class CancelacionReservaSerializer(serializers.ModelSerializer):
+    reserva = ReservaSerializer(read_only=True)
+
     class Meta:
         model = CancelacionReserva
         fields = '__all__'
         read_only_fields = ['fecha_solicitud', 'fecha_respuesta', 'reserva']
+
